738b31aafcda0370fbf014f57b0626604394e5ab
chihtsen <nccu111753155@gmail.com>
2025-01-04 01:48:09 +0800

    feat(Web): making a Hello World web appllication.

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World")
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		n, err := fmt.Fprintf(w, "Hello, World!")
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
+	})
+
+	_ = http.ListenAndServe(":3000", nil)
+	println("Server is running on port 3000")
 }
