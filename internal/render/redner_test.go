6083877e03c7fd414b54fda5e1e304c8cc7579d6
chihtsen <nccu111753155@gmail.com>
2025-02-18 16:23:20 +0800

    feat(web):Writing test for render package I.

---
+++ internal/render/redner_test.go
@@ -0,0 +1,37 @@
+package render
+
+import (
+	"myapp/internal/models"
+	"net/http"
+	"testing"
+)
+
+func TestNewTemplates(t *testing.T) {
+	var td models.TemplateData
+
+	r, err := getSession()
+	if err != nil {
+		t.Error(err)
+	}
+
+	session.Put(r.Context(), "flash", "123")
+
+	result := AddDefaultData(&td, r)
+
+	if result.Flash != "123" {
+		t.Error("expected 123 but got", result.Flash)
+	}
+}
+
+func getSession() (*http.Request, error) {
+	r, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := r.Context()
+	ctx, _ = session.Load(ctx, r.Header.Get("X-Session"))
+	r = r.WithContext(ctx)
+
+	return r, nil
+}
---
+++ internal/render/setup_test.go
@@ -0,0 +1,36 @@
+package render
+
+import (
+	"encoding/gob"
+	"myapp/internal/config"
+	"myapp/internal/models"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+)
+
+var session *scs.SessionManager
+var testApp config.AppConfig
+
+func TestMain(m *testing.M) {
+
+	//what am I going to put in the session
+	gob.Register(models.Reservation{})
+	//change this to true when in production
+	testApp.Inproduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = false
+
+	testApp.Session = session
+
+	app = &testApp
+
+	os.Exit(m.Run())
+}
